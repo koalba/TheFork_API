d9ceceeca70b3dd56d99f3bc2bd3409fa9f1815f
Alba <34134103+koalba@users.noreply.github.com>
2022-05-27 12:27:14 +0200

    Add files via upload

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,5 +1,14 @@
-const bbdd = require('../bbdd')
+const { Restaurant, City, Slider } = require('../schemas/schemas')
 
-function getIndex( req , res ){ res.json({ data : bbdd }) }
+async function getIndex( req , res ){ 
+
+    let r = await Restaurant.find()
+    let c = await Restaurant.find()
+    let s = await Restaurant.find()
+
+    let data = [r, c, s]
+
+    res.status(200).json({ data }) 
+}
 
 module.exports = getIndex
